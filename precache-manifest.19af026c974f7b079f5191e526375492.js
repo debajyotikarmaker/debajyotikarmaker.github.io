e25290e92521ba8d292856c593bd61179f965ce2
Debajyoti karmaker <uqdkarma@gmail.com>
2020-05-22 08:29:22 +0600

    Add files via upload

---
+++ precache-manifest.19af026c974f7b079f5191e526375492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5781578231ee6de9514af166a041f5e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8af819cb1495175916f3",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44df66d51ee9370c3b50",
+    "url": "/test/static/js/2.dacc4c40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.dacc4c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af819cb1495175916f3",
+    "url": "/test/static/js/main.a8b0db23.chunk.js"
+  },
+  {
+    "revision": "bb87ae366e01ebe4e86d",
+    "url": "/test/static/js/runtime-main.1e439a2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
