320dc136d7fd79ecb801f2b7f8a6f7a9372f7265
debajyotikarmaker <uqdkarma@gmail.com>
2020-05-22 08:12:03 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>DEV React hosted!!</h2>
+      <h1>Personal website</h1>
+    </div>
+  )
+}
+
+export default App
